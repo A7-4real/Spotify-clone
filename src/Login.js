a5bde990d66cb1416526a87180ac6e8b81b0c710
A7-4real <a7.4real@gmail.com>
2022-05-05 21:46:24 +0530

    login component

---
+++ src/Login.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Login.css";
+import { loginUrl } from "./spotify";
+
+function Login() {
+  return (
+    <div className="login">
+      {/* Spotify logo */}
+      <img
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt=""
+      />
+
+      {/* Login with Spotify button */}
+      <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
+    </div>
+  );
+}
+
+export default Login;
